01895904a3343bd63f948ae7808d520b4b823ac1
Rakib Hassan <rhbappy456@gmail.com>
2022-12-24 23:46:09 +0600

    create notfound page & footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
 import { Toaster } from 'react-hot-toast';
 import { Header } from './components';
+import { Route, Routes } from 'react-router-dom';
+import { Footer, Home, NotFound } from './pages';
 
 function App() {
   return (
     <>
       <Toaster />
-      <Header/>
+      <Header />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer/>
     </>
   );
 }
